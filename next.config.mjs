2ccc0048e8c1b9ab406361f93a49fbc265c83637
RanuRanjan <ranuranjan25@gmail.com>
2025-01-07 00:36:23 +0530

    image add

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains:["s3.amazonaws.com","via.placeholder.com"]
+    },
+  };
+  
+  export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
 import styles from "./page.module.css";
 import heading from '@/app/style/heading.module.css'
 import style from '@/app/style/heading.module.css'
+import Image from "next/image";
+import Profile from '../../public/vercel.svg'
 export default function Home() {
+  const img= "https://www.google.com/url?sa=i&url=https%3A%2F%2Fgratisography.com%2F&psig=AOvVaw1wn81GbDOId-ZMA7ncxJHd&ust=1736273864270000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPCErara4YoDFQAAAAAdAAAAABAE"
   const {red} =style
   return (
     <div className={styles.page}>
@@ -9,7 +12,16 @@ export default function Home() {
         <h1> Welcome to my website</h1>
         <h2 className={heading.main}>Learning NextJs</h2>
         <h3 className={red}>from Youtube</h3>
+        <Image
+      src="https://via.placeholder.com/150"
+      alt="Placeholder"
+      width={150}
+      height={150}
+    />
+    
+     <img src="https://via.placeholder.com/150" alt="" />
       </main>
     </div>
   );
 }
+
