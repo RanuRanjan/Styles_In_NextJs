35085962dbd003123cfb30e482c2a9882a77fb5d
RanuRanjan <ranuranjan25@gmail.com>
2025-01-07 00:50:49 +0530

    font in nextjs

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,15 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
-        {children}
-      </body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playwrite+AU+SA:wght@100..400&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,13 @@ import heading from '@/app/style/heading.module.css'
 import style from '@/app/style/heading.module.css'
 import Image from "next/image";
 import Profile from '../../public/vercel.svg'
+import {Roboto} from 'next/font/google'
+
+
+const robot= Roboto({
+  weight:'400',
+  subsets:['greek']
+})
 export default function Home() {
   const img= "https://www.google.com/url?sa=i&url=https%3A%2F%2Fgratisography.com%2F&psig=AOvVaw1wn81GbDOId-ZMA7ncxJHd&ust=1736273864270000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPCErara4YoDFQAAAAAdAAAAABAE"
   const {red} =style
@@ -18,8 +25,11 @@ export default function Home() {
       width={150}
       height={150}
     />
-    
      <img src="https://via.placeholder.com/150" alt="" />
+     {/* font */}
+
+     <h4 className={robot.className}>Using Google font</h4>
+   <h4 style={{fontFamily:'Playwrite',fontWeight:"300"}}>font using Link</h4>
       </main>
     </div>
   );
